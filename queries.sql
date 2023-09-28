d7dc2c15ee5a24cb05204a5de7b0aeb86c993179
thasipablo <thasipablo@gmail.com>
2023-09-28 17:11:57 +0100

    update queries

--- queries.sql
+++ queries.sql
@@ -1,15 +1,27 @@
-SELECT * FROM animals WHERE name LIKE '%mon';
+--- SECTION 1
+BEGIN;
+UPDATE animals SET species='unspecified';
+ROLLBACK;
+SELECT * FROM animals;
+---------------------------
+BEGIN;
+UPDATE animals SET species='digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species='pokemon' WHERE species IS NULL;
+SELECT * FROM animals;
+COMMIT;
+SELECT * FROM animals;
+---------------------------
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+---------------------------
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals SET weight_kg = weight_kg * (-1);
+ROLLBACK TO SP1;
+UPDATE animals SET weight_kg = weight_kg * (-1) WHERE weight_kg < 1;
+COMMIT;
 
-SELECT (name) FROM animals WHERE date_of_birth >= '2016-01-01' AND date_of_birth <= '2019-12-31';
-
-SELECT (name) FROM animals WHERE neutered = true AND escape_attempts < 3;
-
-SELECT (date_of_birth) FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
-
-SELECT (name, escape_attempts) FROM animals WHERE weight_kg > 10.5;
-
-SELECT * FROM animals WHERE neutered = true;
-
-SELECT * FROM animals WHERE name NOT IN ('Gabumon');
-
-SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+--- SECTION 2
