76f54a52231e59c6da9b1b60d1a09c96e14805b5
thasipablo <thasipablo@gmail.com>
2023-09-28 13:37:16 +0100

    add species field and make the id auto generated

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
-  id INT, 
+  id INT GENERATED ALWAYS AS IDENTITY, 
   name VARCHAR(50), 
+  species VARCHAR(50),
   date_of_birth DATE, 
   escape_attempts INT, 
   neutered BOOLEAN, 
-  weight_kg DECIMAL
+  weight_kg DECIMAL,
 );
