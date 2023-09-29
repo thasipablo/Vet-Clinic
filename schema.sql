7a5987c8c33abab66fad320014543f6f7f02342d
thasipablo <thasipablo@gmail.com>
2023-09-29 17:33:56 +0100

    add foreign keys and constraints to the animals table

--- schema.sql
+++ schema.sql
@@ -9,8 +9,6 @@ CREATE TABLE animals (
   weight_kg DECIMAL
 );
 
-ALTER TABLE animals ADD PRIMARY KEY (id);
-
 ALTER TABLE animals ADD species VARCHAR(100);
 
 CREATE TABLE owners (
@@ -25,3 +23,10 @@ CREATE TABLE species (
 	name VARCHAR(150),
 	PRIMARY KEY(id)
 );
+
+ALTER TABLE animals ADD PRIMARY KEY (id);
+ALTER TABLE animals DROP species;
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals ADD COLUMN owner_id INT;
+
+ALTER TABLE animals ADD CONSTRAINT species_id FOREIGN KEY (species_id) REFERENCES species (id);
